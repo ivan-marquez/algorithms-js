4d9ad55dac02da7b8c8d5cc52b461e005275d74f
ivan-marquez <ivan.marquez.gdev@gmail.com>
2021-08-23 17:19:05 -0400

    1 file modified, 1 added, 1 deleted

--- udemy/Sorting/mergeSort.js
+++ udemy/Sorting/mergeSort.js
@@ -1,6 +1,10 @@
 /**
  * Merge Sort
  *
+ * Complexity: O(n log n)
+ *
+ * O(log n) decompositions + O(n) comparisons per decomposition
+ *
  * It's a combination of two things - merging and sorting.
  * Exploits the fact that arrays of 0 or 1 element are always sorted.
  * Works by decomposing an array into smaller arrays of 0 or 1 elements, then
@@ -22,3 +26,48 @@
  *   second array into our results and move on to the next value in the second array.
  *   Once we exhaust one array, push in all remaining values from the other array.
  */
+function merge(arr1, arr2) {
+  var results = [];
+
+  var i = 0;
+  var j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  return results;
+}
+
+/**
+ * mergeSort Pseudocode:
+ * Break up the array into halves until you have arrays that are empty or have one element.
+ * Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you
+ * are back at the full length of the array.
+ * Once the array has been merged back together, return the merged (and sorted!) array.
+ */
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  var mid = Math.floor(arr.length / 2);
+  var left = mergeSort(arr.slice(0, mid));
+  var right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
